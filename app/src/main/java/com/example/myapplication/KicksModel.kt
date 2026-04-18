4a7568d888534f4e8d57d23fb480bc843a754976
Fedderli <pokrzywnickikarol@gmail.com>
2026-04-18 19:15:07 +0200

    Stworzenie Bazy danych oraz dodanie pliku google-services.json

---
+++ app/src/main/java/com/example/myapplication/KicksModel.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication
+
+data class KicksModel(
+    val brand: String = "",
+    val imageUrl: String= "",
+    val modelName: String= "",
+    val releaseYear: Int = 0,
+    val resellPrice: Int = 0
+)
